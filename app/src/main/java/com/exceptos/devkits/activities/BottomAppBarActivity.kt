5f07a4e73003bbe88c663b495b0872a841972bf8
Dami Tesfaye <damitesfaye@gmail.com>
2020-04-17 05:17:23 +0100

    75% complete with bottom app bar
    Signed-off-by: Dami Tesfaye <damitesfaye@gmail.com>

--- app/src/main/java/com/exceptos/devkits/activities/BottomAppBarActivity.kt
+++ app/src/main/java/com/exceptos/devkits/activities/BottomAppBarActivity.kt
@@ -2,7 +2,11 @@ package com.exceptos.devkits.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.exceptos.devkits.R
+import com.exceptos.devkits.utils.BottomNavigationDrawer
 
 class BottomAppBarActivity : AppCompatActivity() {
 
@@ -10,4 +14,29 @@ class BottomAppBarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bottom_appbar)
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottom_appbar_menu_primary, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            android.R.id.home -> BottomNavigationDrawer().show(supportFragmentManager, BottomNavigationDrawer().tag)
+            R.id.app_bar_search -> toast("Search menu item is clicked!")
+            R.id.app_bar_mail -> toast("Fav menu item is clicked!")
+            R.id.app_bar_archieve -> toast("Settings item is clicked!")
+            R.id.app_bar_delete -> toast("Settings item is clicked!")
+        }
+        return true
+    }
+    private fun toast(s: String) {
+        Toast.makeText(this,s,Toast.LENGTH_SHORT).show()
+    }
+
+
 }
+
+
+
+
